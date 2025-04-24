915c9cbc0fbde5588c6361e959ab9aec26701d9e
Pedro Meneghel <pedromeneghel@gmail.com>
2025-04-24 11:24:27 +0000

    feat: adiciona relacionamentos

--- src/db/schema/restaurants.ts
+++ src/db/schema/restaurants.ts
@@ -1,7 +1,7 @@
 import { createId } from '@paralleldrive/cuid2';
 import { relations } from 'drizzle-orm';
 import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
-import { users } from './users';
+import { orders, products, users } from '.';
 
 export const restaurants = pgTable("restaurants", {
   id: text("id").$defaultFn(() => createId()).primaryKey(),
@@ -12,12 +12,14 @@ export const restaurants = pgTable("restaurants", {
   updated_at: timestamp("updated_at").notNull().defaultNow()
 });
 
-export const restaurantsRelations = relations(restaurants, ({ one }) => {
+export const restaurantsRelations = relations(restaurants, ({ one, many }) => {
   return {
-    mangager: one(users, {
+    manager: one(users, {
       fields: [restaurants.managerId],
       references: [users.id],
       relationName: 'restaurant_manager'
-    })
+    }),
+    orders: many(orders),
+    products: many(products)
   }
 });
--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,5 +1,7 @@
 import { createId } from '@paralleldrive/cuid2';
+import { relations } from 'drizzle-orm';
 import { pgEnum, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { orders, restaurants } from '.';
 
 export const userRoleEnum = pgEnum('user_role', ['manager', 'customer']);
 
@@ -11,4 +13,15 @@ export const users = pgTable("users", {
   role: userRoleEnum("role").default('customer').notNull(),
   created_at: timestamp("created_at").notNull().defaultNow(),
   updated_at: timestamp("updated_at").notNull().defaultNow()
+});
+
+export const usersRelations = relations(users, ({ one, many }) => {
+  return {
+    managerRestaurant: one(restaurants, {
+      fields: [users.id],
+      references: [restaurants.managerId],
+      relationName: 'manager_restaurant'
+    }),
+    orders: many(orders)
+  }
 });
